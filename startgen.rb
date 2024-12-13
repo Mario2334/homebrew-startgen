e7cbe1744bfd0c143b9cb73d34b8351ce6eaf1d7
sanket <sanketm221995@gmail.com>
2024-12-13 22:33:46 +0530

    Added new release

--- startgen.rb
+++ startgen.rb
@@ -2,7 +2,7 @@ class Startgen < Formula
   desc "A CLI tool to generate boilerplate projects using natural language"
   homepage "https://github.com/mario2334/startgen"
   url "https://github.com/Mario2334/startgen/releases/download/v0.1.1-alpha/startgen-darwin-amd64.zip" # Update for your release
-  sha256 "1bf2c402438fcad2137f910729dcda583e167d01f2b00ee699eb5d3cc04117d6" # Calculate using 'shasum -a 256 <file>'
+  sha256 "3f81085864b21f6fe5740ba594e7c036d5484a8dabf7423c5f4f3b21e3920cd7" # Calculate using 'shasum -a 256 <file>'
   version "0.1.1-alpha"
 
   def install
