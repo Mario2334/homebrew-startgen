1f2b0c40939ac8476741a469faa633bc481b0394
Sanket Mokashi <sanket@Sankets-MacBook-Pro.local>
2024-12-13 22:17:35 +0530

    Add Homebrew formula for StartGen

--- startgen.rb
+++ startgen.rb
@@ -2,7 +2,7 @@ class Startgen < Formula
   desc "A CLI tool to generate boilerplate projects using natural language"
   homepage "https://github.com/mario2334/startgen"
   url "https://github.com/Mario2334/startgen/releases/download/v0.1.1-alpha/startgen-darwin-amd64.zip" # Update for your release
-  sha256 "be17add25d763c22bd8e1487406b3e2fa4ee61f61cd23199f9d7360478d4f375" # Calculate using 'shasum -a 256 <file>'
+  sha256 "1bf2c402438fcad2137f910729dcda583e167d01f2b00ee699eb5d3cc04117d6" # Calculate using 'shasum -a 256 <file>'
   version "0.1.1-alpha"
 
   def install
